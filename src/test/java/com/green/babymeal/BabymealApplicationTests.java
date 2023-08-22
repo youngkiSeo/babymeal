caf0e7a918f7614b47fd6c411d95692ca03575ad
youngkiSeo <matep8@knou.ac.kr>
2023-08-22 16:15:10 +0900

    3차 프로젝트 - 서영기

---
+++ src/test/java/com/green/babymeal/BabymealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.babymeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabymealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
