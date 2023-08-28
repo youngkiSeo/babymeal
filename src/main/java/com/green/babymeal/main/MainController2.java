d2296fb0c67680ad7829452093f5350dfe169941
홍기윤 <wlwl753@naver.com>
2023-08-28 17:53:03 +0900

    Main

--- src/main/java/com/green/babymeal/main/MainController2.java
+++ src/main/java/com/green/babymeal/main/MainController2.java
@@ -2,6 +2,8 @@ package com.green.babymeal.main;
 
 import com.green.babymeal.main.model.MainSelPaging;
 import com.green.babymeal.main.model.SelDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/main")
 @RequiredArgsConstructor
+@Tag(name = "메인 페이지")
 public class MainController2 {
 
     private final MainService service;
 
 
   @GetMapping
+  @Operation(summary = "메인페이지 상품",description = ""+
+  "check:1 (기본으로 보여줄 상품)<br>"+
+  "check:2 (회원 자녀의 개월에 따라 상품 추천)<br>"+
+  "check:3 (랜덤으로 상품 추천)<br>"+
+  "check:4 (제일 많이 팔린 상품)<br>"+
+  "check:5 (제일 많이 팔린 상품 더보기)<br>"+
+  "page: 페이지 번호"+
+  "row: 한페이지에 보일 상품의 갯수")
   public MainSelPaging mainSel(SelDto dto){
       return service.mainSel(dto);
   }
--- src/main/java/com/green/babymeal/main/MainMapper.java
+++ src/main/java/com/green/babymeal/main/MainMapper.java
@@ -8,10 +8,18 @@ import java.util.List;
 
 @Mapper
 public interface MainMapper {
-    int SelMainCount();
-    List<MainSelVo> SelMainVo(int startIdx,int row);
+    int selMainCount();
+    List<MainSelVo> selMainVo(int startIdx,int row);
 
     int birth(Long iuser);
     List<MainSelVo> birthRecommendFilter(int cateId,int row);
+
+    List<MainSelVo> random();
+
+    List<MainSelVo> bestSel();
+    int bestSelAllCount();
+    List<MainSelVo> bestSelAll(int startIdx,int row);
+
+    Long levelSel(Long productId);
 
 }
