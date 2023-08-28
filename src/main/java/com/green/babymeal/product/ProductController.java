c0447a46800036cdeab97f14a4996453c0d4b36f
김다율 <kdy12061004@gmail.com>
2023-08-28 09:42:16 +0900

    리뷰작성-김다율

--- src/main/java/com/green/babymeal/product/ProductController.java
+++ src/main/java/com/green/babymeal/product/ProductController.java
@@ -32,12 +32,12 @@ public class ProductController {
         return service.postReview(dto);
     }
 
-//    @GetMapping("/review/{productId}")
-//    @Operation(summary = "상품번호에 맞는 리뷰 조회", description = ""+
-//            "productId = 상품코드 <br>" +
-//            "페이지당 10개씩 출력" +
-//            "테스트시 참고 : 존재하는 상품번호 예시 1 6")
-//    List<ReviewEntity> selReview(@PathVariable Long productId){
-//        return service.getReviewById(productId);
-//    }
+    @GetMapping("/review/{productId}")
+    @Operation(summary = "상품번호에 맞는 리뷰 조회", description = ""+
+            "productId = 상품코드 <br>" +
+            "페이지당 10개씩 출력" +
+            "테스트시 참고 : 존재하는 상품번호 예시 1 6")
+    List<ReviewEntity> selReview(@PathVariable Long productId){
+        return service.getReviewById(productId);
+    }
 }
