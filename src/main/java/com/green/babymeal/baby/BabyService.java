6516062be01c3ff0e8458c356160a5c2080e1d3d
홍기윤 <wlwl753@naver.com>
2023-08-29 16:53:52 +0900

    장바구니

--- src/main/java/com/green/babymeal/baby/BabyService.java
+++ src/main/java/com/green/babymeal/baby/BabyService.java
@@ -20,7 +20,7 @@ public class BabyService {
     public BabyInsVo insBaby(BabyInsDto dto){
         UserBabyinfoEntity entity = new UserBabyinfoEntity();
         entity.setBirthday(dto.getBirthday());
-        entity.setPrefer(dto.getPrefer());
+//        entity.setPrefer(dto.getPrefer());
 
         UserEntity entity1 = new UserEntity();
         entity1.setIuser(dto.getIuser());
@@ -33,7 +33,7 @@ public class BabyService {
 
         return BabyInsVo.builder()
                 .birthday(entity.getBirthday())
-                .prefer(entity.getPrefer())
+//                .prefer(entity.getPrefer())
                 .iuser(entity1.getIuser())
                 .babyId(userBabyalleEntity.getBabyallergy())
                 .build();
