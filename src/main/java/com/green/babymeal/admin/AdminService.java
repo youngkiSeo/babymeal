e1d3e6dfd38ba603bc8a3b9317f8ce7b3d8dcd7b
김다율 <kdy12061004@gmail.com>
2023-08-30 18:11:16 +0900

    관리자/주문내역조회-김다율

---
+++ src/main/java/com/green/babymeal/admin/AdminService.java
@@ -0,0 +1,25 @@
+package com.green.babymeal.admin;
+
+import com.green.babymeal.common.entity.OrderlistEntity;
+import com.green.babymeal.common.repository.OrderlistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private OrderlistRepository orderlistRepository;
+
+
+    public Page<OrderlistEntity> selOrder(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable) {
+        return orderlistRepository.findByCreatedAtBetween(startDate, endDate, pageable);
+    }
+}
