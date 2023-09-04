8456ccc7f16b87ed106263444996b156e8fe5a35
aswd456 <wlwl753@naver.com>
2023-09-04 16:02:17 +0900

    category 커밋

--- src/main/java/com/green/babymeal/cate/model/Cate.java
+++ src/main/java/com/green/babymeal/cate/model/Cate.java
@@ -1,10 +1,15 @@
 package com.green.babymeal.cate.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Setter
+@Getter
+@Builder
 public class Cate {
     private Category category;
     private List<CateViewRepositoryDetail> cateDetail;
--- src/main/java/com/green/babymeal/cate/model/Category.java
+++ src/main/java/com/green/babymeal/cate/model/Category.java
@@ -1,8 +1,13 @@
 package com.green.babymeal.cate.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@Builder
 public class Category {
     private Long cateId;
     private String cateName;
