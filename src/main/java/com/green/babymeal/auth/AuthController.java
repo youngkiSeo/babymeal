c5ddd7ca17628fb93c144a81f28cdfe7ade370eb
이진규 <dlwlsrb0307@naver.com>
2023-09-04 14:49:44 +0900

    로그아웃 버튼방식으로 변경, 태그설정

--- src/main/java/com/green/babymeal/auth/AuthController.java
+++ src/main/java/com/green/babymeal/auth/AuthController.java
@@ -5,21 +5,27 @@ import com.green.babymeal.auth.model.SignInReqDto;
 import com.green.babymeal.auth.model.SignUpReqDto;
 import com.green.babymeal.auth.model.SignUpResultDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/sign-api")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "회원 페이지")
 public class AuthController {
     private final AuthService service;
 
     @PostMapping("/sign-up")
+    @Operation(summary = "회원가입",
+            description = "가입하려는 유저의 정보를 넣으면 됩니다.<br>" +
+                    "※ 가입 성공시 \"가입에 성공하셨어요\" 메시지 나옴")
     public ResponseEntity<SignUpResultDto> postSignUp(@RequestBody SignUpReqDto dto
             , HttpServletRequest req
             , HttpServletResponse res) {
@@ -31,6 +37,9 @@ public class AuthController {
     }
 
     @PostMapping("/sign-in")
+    @Operation(summary = "로그인",
+            description = "해당유저의 아이디 , 비밀번호를 넣으면 됩니다.<br>" +
+                    "※ 소셜때문에 이메일에서 아이디 방식으로 바뀜")
     public ResponseEntity<AuthResVo> postSignIn(@RequestBody SignInReqDto dto
             , HttpServletRequest req
             , HttpServletResponse res) {
@@ -39,6 +48,9 @@ public class AuthController {
     }
 
     @PostMapping("/admin/sign-in")
+    @Operation(summary = "관리자 로그인",
+            description = "해당유저의 아이디 , 비밀번호를 넣으면 됩니다.<br>" +
+                    "※ 소셜때문에 이메일에서 아이디 방식으로 바뀜")
     public ResponseEntity<AuthResVo> adminPostSignIn(@RequestBody SignInReqDto dto
             , HttpServletRequest req
             , HttpServletResponse res) {
@@ -47,14 +59,31 @@ public class AuthController {
     }
 
     @GetMapping("/sign-out")
+    @Operation(summary = "로그아웃",
+            description = "해당유저 로그아웃<br>" +
+                    "※ 리프레시 토큰을 바로 삭제를 한다음 엑세스토큰은 블랙리스트에 추가함")
+    public ResponseEntity getSignout(HttpServletRequest req
+            , HttpServletResponse res) {
+        service.signOut(req, res);
+//        ResponseCookie responseCookie = ResponseCookie.from("access-token");
+        return ResponseEntity.ok(1);
+    }
+
+    /*@GetMapping("/sign-out")
+    @Operation(summary = "로그아웃",
+            description = "엑세스토큰을 넣으면 해당유저가 로그아웃<br>" +
+                    "※ 소셜때문에 이렇게 한것으로 알고있어요")
     public ResponseEntity getSignout(@RequestParam(required = false) String accessToken
             , HttpServletRequest req
             , HttpServletResponse res) {
         service.signOut(accessToken, req, res);
         return ResponseEntity.ok(1);
-    }
+    }*/
 
     @GetMapping("/refresh")
+    @Operation(summary = "엑세스 토큰 재발급",
+            description = "버튼을 누를 시 바로 됨<br>" +
+                            "※ 로그인 해야 되요")
     public ResponseEntity<AuthResVo> getRefresh(HttpServletRequest req) {
         AuthResVo vo = service.refresh(req);
         return ResponseEntity.ok(vo);
@@ -64,7 +93,7 @@ public class AuthController {
     // -----------------비밀번호 찾기
 
     @PostMapping("/id")
-    @Operation(summary = "id 중복체크",
+    @Operation(summary = "아이디 중복체크",
             description = "return : 1이면 중복인것")
     public int emailCheck(@RequestParam String uid){
         return service.uidCheck(uid);
--- src/main/java/com/green/babymeal/baby/BabyController.java
+++ src/main/java/com/green/babymeal/baby/BabyController.java
@@ -3,20 +3,22 @@ package com.green.babymeal.baby;
 import com.green.babymeal.baby.model.BabyInsDto;
 import com.green.babymeal.baby.model.BabyInsVo;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/baby")
+@RequestMapping("/api/baby")
+@Tag(name = "아기 페이지")
 @RequiredArgsConstructor
 public class BabyController {
 
     private final BabyService service;
 
     @PostMapping
-    @Operation(summary = "아기정보",description = "" +
+    @Operation(summary = "아기정보 등록",description = "" +
             "childBirth : 아기의 생년월일<br>" +
             "prefer : 기호식품<br>" +
             "allegyId : 아이의 알러지 정보( , 로 구분을 해야 합니다.) (ex : 1,2,3,4)<br>" +
