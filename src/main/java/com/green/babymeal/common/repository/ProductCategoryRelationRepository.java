17ec0f9b86f9f110f1db69a5292db4551a450253
youngkiSeo <matep8@knou.ac.kr>
2023-09-04 18:40:08 +0900

    mypage orderlist 오류 수정 - 서영기

--- src/main/java/com/green/babymeal/common/repository/ProductCategoryRelationRepository.java
+++ src/main/java/com/green/babymeal/common/repository/ProductCategoryRelationRepository.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 @Repository
 public interface ProductCategoryRelationRepository extends JpaRepository<ProductCateRelationEntity, Long> {
    ProductCateRelationEntity findByProductEntity(ProductEntity entity);
+
    ProductCateRelationEntity findFirstByProductEntity(ProductEntity entity);
    // 상품과 관련된 categoryEntity > cateDetailEntity >  cateDetailId를 가져오는 메소드
 //   @Query("SELECT r.categoryEntity.cateId FROM ProductCateRelationEntity r WHERE r.productEntity = :productEntity")
